9f332b2aa3833cc1c46488fd85d98657b494910f
Luiz Antonio <luizsanq@gmail.com>
2023-05-27 19:03:25 -0300

    Add initialization shortcut

--- start.bat
+++ start.bat
@@ -1,2 +1,13 @@
-node server/index.js
-pause
+@echo off
+
+cd server
+start "" /b /wait cmd /c "npm install"
+start "" cmd /c "npm run start"
+cd ..
+
+cd client
+start "" /b /wait cmd /c "npm install"
+start "" cmd /c "npm run host"
+cd ..
+
+exit
