89201d7f2047888545d223d30a90d071212fa1b7
Luizsan <luizsanq@gmail.com>
2024-03-04 13:45:44 -0300

    Add new claude models

--- server/api/anthropic.js
+++ server/api/anthropic.js
@@ -16,6 +16,8 @@ class Anthropic{
             title: "Model",
             description: "The model that will complete your prompt. This parameter controls which version of Claude answers your request.",
             type: "select", default: "claude-v1.2", choices: [
+                "claude-3-opus-20240229",
+                "claude-3-sonnet-20240229",
                 "claude-2",
                 "claude-v1",
                 "claude-v1-100k",
