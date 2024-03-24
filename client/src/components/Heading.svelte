65d6a3377439aa43bd5ada207979c5c37a62421d
Luizsan <luizsanq@gmail.com>
2024-03-24 19:13:13 -0300

    More options to heading component

--- client/src/components/Heading.svelte
+++ client/src/components/Heading.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
     export let title : string = ""
     export let description : string = ""
+    export let scale : number = 1;
+    export let reverse : boolean = false;
 </script>
 
 {#if title}
-    <div class="section">
-        <div>
-            <div class="title">{title}</div>
-            {#if description}
-                <div class="explanation">{description}</div>
-            {/if}
-        </div>
+    <div class="section" class:reverse={reverse}>
+        <div class="title" style="font-size: {scale * 100}%">{title}</div>
+        {#if description}
+            <div class="explanation">{description}</div>
+        {/if}
     </div>
 {/if}
+
+<style>
+    .section{
+        gap: 0px;
+    }
+
+    .reverse{
+        flex-flow: column-reverse;
+    }
+</style>
