928468b00bbf70abfe554beebff74b13909da207
Luizsan <luizsanq@gmail.com>
2024-04-07 10:58:39 -0300

    Prevent gpt tokenizer crash

--- server/tokenizer/gpt.js
+++ server/tokenizer/gpt.js
@@ -2,9 +2,14 @@ import { getEncoding, getEncodingNameForModel } from "js-tiktoken";
 
 export function getTokens(text, model){
     if(!text) return 0
-    const name = getEncodingNameForModel(model || "gpt-3.5-turbo")
-    const encoder = getEncoding(name)
-    return encoder.encode(text)?.length ?? 0
+    try{
+        const name = getEncodingNameForModel(model || "gpt-3.5-turbo")
+        const encoder = getEncoding(name)
+        return encoder.encode(text)?.length ?? 0
+    }catch(error){
+        console.log(error)
+        return 0
+    }
 }
 
 export default { getTokens }
