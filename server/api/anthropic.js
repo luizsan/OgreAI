2a584ffc7e1eaa85ad1a25f34cc9ace5d33a6b62
Luizsan <luizsanq@gmail.com>
2025-03-03 03:55:34 -0300

    Updated anthropic models

--- server/api/anthropic.js
+++ server/api/anthropic.js
@@ -17,14 +17,12 @@ class Anthropic{
             title: "Model",
             description: "The model that will complete your prompt. This parameter controls which version of Claude answers your request.",
             type: "select", default: "claude-v1.2", choices: [
+                "claude-3-7-sonnet-latest",
                 "claude-3-5-sonnet-latest",
                 "claude-3-5-haiku-latest",
                 "claude-3-opus-latest",
                 "claude-3-sonnet-20240229",
                 "claude-3-haiku-20240307",
-                "claude-2.1",
-                "claude-2.0",
-                "claude-instant-1.2"
             ]
         },
 
