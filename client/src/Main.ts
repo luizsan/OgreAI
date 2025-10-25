3496f42dd661c43e7a8990d4b7bb5afbefcd802b
Luizsan <luizsanq@gmail.com>
2025-10-25 14:55:32 -0300

    Fixed viewport scaling on Chrome(ium?) mobile

--- client/src/Main.ts
+++ client/src/Main.ts
@@ -1,13 +1,24 @@
 import './css/const.css'
 import './css/main.css'
 import './css/layout.css'
-import './css/theme.css'
 import './css/colors.css'
 import './css/components.css'
 import './css/scrollbar.css'
 import './css/overrides.css'
 
 import App from './views/main/App.svelte'
+
+// Chrome mobile requires this to correctly scale the page.
+const viewport = document.querySelector('meta[name="viewport"]');
+if (viewport) {
+    const scale = 1.0 / window.devicePixelRatio * 2.0;
+    viewport.setAttribute('content',
+        `width=device-width,
+        initial-scale=${scale},
+        minimum-scale=${scale},
+        maximum-scale=${scale}`
+    );
+}
 
 const app = new App({
   target: document.getElementById('app'),
