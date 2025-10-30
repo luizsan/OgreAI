ea4e3b821319349056e44bb76afeee71b281cd78
Luizsan <luizsanq@gmail.com>
2025-10-30 20:38:22 -0300

    Adjustments to theme boxes, containers, checkboxes and API status

--- client/src/views/settings/Status.svelte
+++ client/src/views/settings/Status.svelte
@@ -19,20 +19,28 @@
 
     .badge{
         border-radius: 50px;
-        padding: 2px 12px;
+        padding: 0px 12px;
         font-size: 75%;
         background: hsla(0, 0%, 10%, 75% );
         font-weight: 700;
         word-spacing: 4px;
     }
 
+    .badge::before{
+        content: "\25CF";
+        margin-right: 6px;
+    }
+
     .disabled{
         color: hsl(0, 0%, 85%);
     }
 
-    .badge::before{
-        content: "\25CF";
-        margin-right: 6px;
+    .danger{
+        color: var( --content-danger-300 );
+    }
+
+    .confirm{
+        color: var( --content-confirm-300 );
     }
 
 </style>
